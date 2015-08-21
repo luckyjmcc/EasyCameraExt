35ad3edd313327410d3cfc1344d0c7bad9fc61f8
iloveaman <iloveaman@163.com>
2015-08-21 09:41:04 +0800

    修复魅族（4:3分辨率奔溃问题）

--- src/main/java/net/bozho/easycamera/CameraParaUtil.java
+++ src/main/java/net/bozho/easycamera/CameraParaUtil.java
@@ -11,9 +11,10 @@ import java.util.List;
  * Created by admin on 2015/7/29.
  */
 public class CameraParaUtil {
-    private static final String tag = "yan";
+    private static final String tag = "CameraParaUtil";
     private CameraSizeComparator sizeComparator = new CameraSizeComparator();
     private static CameraParaUtil myCamPara = null;
+    public static float mRate = 1.33f;
 
     private CameraParaUtil() {
 
@@ -33,11 +34,15 @@ public class CameraParaUtil {
 
         int i = 0;
         for (Size s : list) {
-            if ((s.width > th) && equalRate(s, 1.33f)) {
+            //if ((s.width > th) && equalRate(s, 1.33f)) {//除保证比率外，还保证用户需要设置的尺寸宽度最小值。这个大家根据需要随便改。
+            if ((s.width > th) && equalRate(s, mRate)) {//除保证比率外，还保证用户需要设置的尺寸宽度最小值。这个大家根据需要随便改。
                 Log.i(tag, "最终设置预览尺寸:w = " + s.width + "h = " + s.height);
                 break;
             }
             i++;
+        }
+        if(i==list.size()){
+            i=0;
         }
 
         return list.get(i);
@@ -45,10 +50,10 @@ public class CameraParaUtil {
 
     public Size getPictureSize(List<Size> list, int th) {
         Collections.sort(list, sizeComparator);
-
         int i = 0;
         for (Size s : list) {
-            if ((s.width > th) && equalRate(s, 1.33f)) {
+            //if ((s.width > th) && equalRate(s, 1.33f)) {
+            if ((s.width > th) && equalRate(s, mRate)) {
                 Log.i(tag, "最终设置图片尺寸:w = " + s.width + "h = " + s.height);
                 break;
             }
@@ -58,6 +63,12 @@ public class CameraParaUtil {
         return list.get(i);
     }
 
+    /**
+     * 保证Size的长宽比率。一般而言这个比率为1.333/1.7777即通常说的4:3和16:9比率。
+     * @param s
+     * @param rate
+     * @return
+     */
     public boolean equalRate(Size s, float rate) {
         float r = (float) (s.width) / (float) (s.height);
         if (Math.abs(r - rate) <= 0.2) {
